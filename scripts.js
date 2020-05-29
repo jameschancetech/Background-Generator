bd1207b34303842227acea7e2ac567cf73420198
James Chance <38363488+jameschancetech@users.noreply.github.com>
2020-05-29 10:13:43 +0100

    Challenges done
    Random button added and working
    Colors now show at page load

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ var css = document.querySelector(`h3`);
 var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 var body = document.getElementById(`gradient`);
+var randomButton = document.querySelector('.random-button');
 
 function setGradient() {
 	body.style.background = 
@@ -10,7 +11,6 @@ function setGradient() {
 	+ `, ` 
 	+ color2.value 
 	+ `)`;
-
 	css.textContent = body.style.background + `;`;
 }
 
@@ -18,11 +18,28 @@ color1.addEventListener(`input`,setGradient);
 color2.addEventListener(`input`, setGradient);
 
 
+function randomHexValue() {
+	return  `#` + Math.floor(Math.random()*16777215).toString(16);
+}
+
+
+randomButton.addEventListener(`click`, ()=> {
+	color1.value = randomHexValue();
+	color2.value = randomHexValue();
+	setGradient();
+})
+
+setGradient()
+
+
+
+
+
 /* 
 
-1. Write code so that the colour inputs match the background generated on the first page load. 
+✔️ 1. Write code so that the colour inputs match the background generated on the first page load. 
 
-2. Display the initial CSS linear gradient property on page load.
+✔️ 2. Display the initial CSS linear gradient property on page load.
 
 3. BONUS: Add a random button which generates two random numbers for the colour inputs.
 
